1a3023cdc4f3f75e6644bac6304b8e638f256db4
cmo <fourty2@126.com>
2024-04-30 18:05:09 +0800

    Plotting tools

--- Runners/Trainer.py
+++ Runners/Trainer.py
@@ -8,6 +8,7 @@ import pandas as pd
 from collections import namedtuple
 from Runners.BaseRunner import BaseRunner
 from Tools.Dataset import Dataset
+from utils import utils
 
 log = logging.getLogger(__name__)
 
@@ -59,7 +60,7 @@ class Trainer(BaseRunner):
 
         epoch_start, epoch_end = self.init_epoch_id, self.init_epoch_id+self.num_epochs
         for epoch in range(epoch_start, epoch_end):
-            log.info(f"Epoch: {epoch+1}/{epoch_end}.")
+            log.info(f"Epoch: {epoch}/{epoch_end-1}.")
             # train one epoch
             log.info(f"Training...")
             start_time = time.time()
@@ -86,6 +87,7 @@ class Trainer(BaseRunner):
             self.end_epoch(epoch=epoch, output=output_save)
             log.info(f"Complete in {(time.time()-start_time)/60.:.2f} min.")
             log.info(f"Total time: {(time.time()-self.time_start)/60.:.2f} min.")
+            log.info('\n\n\n')
 
         # Test model 
         start_time = time.time()
@@ -104,8 +106,11 @@ class Trainer(BaseRunner):
     Running at the end of each epoch
     """
     def end_epoch(self, epoch, output):
-        np.save(f"{self.save_dir}/valset_output.npy",arr=output.numpy())
+        output = output.numpy()
+        np.save(f"{self.save_dir}/valset_output.npy",arr=output)
         self.save(record_path=f'{self.save_dir}/run_record.csv', model_path=f'{self.save_dir}/net_{epoch}.pt')
+        self.plot_loss()
+        self.plot_score(output=output)
 
         # Record best epoch
         cur_epoch_result = self.search_record(self.run_record, epoch=epoch, batch_type='validation')
@@ -124,3 +129,44 @@ class Trainer(BaseRunner):
 
     def finish(self):
         self.save(record_path=f'{self.save_dir}/run_record.csv', model_path=f'{self.save_dir}/net.pt')
+    
+    def plot_loss(self):
+        result = pd.DataFrame(self.run_record)
+        batch_types = result['batch_type'].unique()
+        result['loss'] = result['loss'] * result['batch_weight']
+        plot = utils.PlotContainer(xlabel='Epoch', ylabel='Loss',figname=f'{self.save_dir}/loss_to_epoch.pdf')
+        result = result.groupby(['batch_type', 'epoch']).sum()
+        result['loss'] = result['loss'] / result['batch_weight']
+
+        for btype in batch_types:
+            cur_result = result.loc[btype]
+            plot.ax.plot(cur_result.index, cur_result['loss'], linewidth=2, label=f"{btype} loss", marker='o')
+        plot.apply_settings(if_legend=True)
+        plot.savefig()
+    
+    def plot_score(self, output):
+        from scipy.special import softmax
+        from sklearn.metrics import roc_curve
+        weight = output[:,0]
+        truth = output[:,1]
+        score = softmax(output[:, 2:], axis=1)[:,1]
+        signal_mask = (truth==1)
+
+        # Plot scores
+        plot = utils.PlotContainer(xlabel='Score', ylabel='A.U.', figname=f'{self.save_dir}/scores.pdf')
+        bins = 50
+        plot.ax.hist(score[signal_mask], bins=bins, weights=weight[signal_mask], label='Signal', color='red', histtype='step', linewidth=2)
+        plot.ax.hist(score[~signal_mask], bins=bins, weights=weight[~signal_mask], label='Background', color='blue', histtype='step', linewidth=2)
+        plot.apply_settings(if_legend=True)
+        plot.savefig()
+
+        # Plot ROC
+        fpr, tpr, _ = roc_curve(truth, score, sample_weight=weight)
+        roc_auc = np.trapz(tpr, fpr)
+        log.info(f"auc: {roc_auc}.")
+        plot = utils.PlotContainer(xlabel='False Positive Rate', ylabel='True Positive Rate', figname=f'{self.save_dir}/roc.pdf', xlim=[0,1], ylim=[0,1])
+        plot.ax.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.3f)' % roc_auc)
+        plot.ax.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+        plot.ax.legend(loc='lower right')
+        plot.apply_settings()
+        plot.savefig()
