b642e1cd494e797c8219b5889ffe953f4b9162ea
cmo <fourty2@126.com>
2024-05-19 16:34:17 +0800

    Refactor recorder

--- Runners/BaseRunner.py
+++ Runners/BaseRunner.py
@@ -4,92 +4,61 @@ import pandas as pd
 import torch
 import logging
 import time
-from Tools.Evaluator import Evaluator
-from Tools.Network import NetworkWrapper
+from Tools.Recorder import Recorder
+from Metrics.MetricInterface import MetricInterface
+from Datasets.DatasetInterface import DatasetInterface
+from Networks.NetworkInterface import NetworkInterface
 
 log = logging.getLogger(__name__)
 
-class BaseRunner(ABC):
+class BaseRunner(ABC, Recorder):
     def __init__(self, config):
-        self.time_start = time.time()
         self.config = config
         self.load()
-        os.makedirs(self.save_dir + '/models/', exist_ok=True)
 
-    @abstractmethod
     def load(self):
-        self.runner_config = self.config.get('runner_config')
-        self.batch_size = self.config.get('batch_size')
-        self.device = self.config.get('device')
-        self.save_dir = self.config.get("save_dir")
-        self.load_model()
-        self.load_evaluator()
-        self.load_data()
-        self.load_run_record()
-
-    def load_model(self):
-        self.network_wrapper = NetworkWrapper(self.config.get('network_config'), pre_model_path=None)
-        self.network_wrapper.to(self.config.get('device'))
-
-    def load_evaluator(self):
-        self.evaluator = Evaluator(self.config.get('evaluator_config'))
-
-    @abstractmethod
-    def load_data(self):
-        ...
-
-    def load_run_record(self, run_record_path=None):
-        if run_record_path:
-            prev_record = pd.read_csv(run_record_path)
-            self.run_record = prev_record.to_dict('list')
-            self.init_epoch_id = self.run_record.get('epoch')[-1] + 1
-        else:
-            self.run_record = {
-                'epoch': [],
-                'batch_type': [], # train validation or test
-                'batch_id': [],
-                'batch_weight': [],
-            }
-            for key in self.evaluator.Critria._fields:
-                self.run_record[key] = []
-
-    def do_record(self, **kwds):
-        for key, val in kwds.items():
-            if type(val) == type(torch.tensor(0)):
-                val = val.item()
-            self.run_record[key].append(val)
-
-    @staticmethod
-    def search_record(record, epoch, batch_type):
-        record = pd.DataFrame(record)
-        return record.loc[(record['epoch']==epoch)&(record['batch_type']==batch_type)].to_dict('list')
-
-    def print_epoch_result(self, epoch):
-        epoch_result = pd.DataFrame(self.run_record)
-        epoch_result = epoch_result.loc[epoch_result['epoch']==epoch]
-        epoch_result = epoch_result.set_index('batch_type')
-        for batch_type in epoch_result.index.unique():
-            cur_result = epoch_result.loc[batch_type]
-            cur_weight = cur_result['batch_weight']
-            msg = f"Type: {batch_type}. Mean result: "
-            for key in self.evaluator.Critria._fields:
-                msg += f"{key}={(cur_result[key]*cur_weight).sum()/cur_weight.sum():.4f}\t"
-            log.info(msg=msg)
+        self.dataset = DatasetInterface(self.config.get('data_config'))
+        metric = MetricInterface(self.config.get('metric_config'))
+        network = NetworkInterface(self.config.get('network_config'))
+        super(Recorder, self).__init__(self.config, metric, network)
 
     def apply_model(self, data_loader, epoch=0, batch_type='test'):
+        output_save, truth_label_save, weight_save = [], [], []
         output_save = []
-        self.network_wrapper.eval()
+        self.network.eval()
         with torch.no_grad():
             for batch_id, batch in enumerate(data_loader, 0):
                 # Model output
                 batch = batch.to(self.device)
                 truth_label=batch.y
-                output = self.network_wrapper(batch)
-                result = self.evaluator(output=output, truth_label=truth_label, weight=batch.weight)
-                # record result
-                self.do_record(epoch=epoch, batch_type=batch_type, batch_id=batch_id, batch_weight=batch.weight.sum(), **result._asdict())
-                output_save.append(torch.cat([batch.weight.view(-1,1), truth_label.view(-1, 1), output ], 1).detach().cpu())
-        return torch.cat(output_save).numpy()
+                output = self.network(batch)
+                loss, metrics = self.metric(output=output, truth_label=truth_label, weight=batch.weight_train)
+                self.end_of_batch(epoch=epoch, batch_type=batch_type, batch_id=batch_id, batch_weight=batch.weight_train.sum(), learning_rate=0, loss=loss.item(), metrics=metrics)
+
+                output_save.append(output.detach().cpu())
+                truth_label_save.append(truth_label.detach().cpu())
+                weight_save.append(batch.weight.detach().cpu())
+        return torch.cat(output_save), torch.cat(truth_label_save), torch.cat(weight_save)
+    
+    def train_model(self, data_loader, epoch):
+        if not hasattr(self, "optimizer"):
+            log.info("No optimizer found, create a new one.")
+            self.optimizer = torch.optim.Adam(self.network.parameters(), lr=self.config.get('learning_rate'))
+
+        self.network.train()
+        for batch_id, batch in enumerate(data_loader, 0):
+            # clear the gradients of all optimized variables
+            self.optimizer.zero_grad()
+            # Model output
+            batch = batch.to(self.device)
+            output = self.network(batch)
+            truth_label=batch.y
+            loss, metrics = self.metric(output=output, truth_label=truth_label, weight=batch.weight_train)
+            # backward pass: compute gradient of the loss with respect to model parameters
+            loss.backward()
+            self.optimizer.step()
+            # record result
+            self.end_of_batch(epoch=epoch, batch_type='train', batch_id=batch_id, batch_weight=batch.weight.sum(), learning_rate=self.optimizer.param_groups[0]['lr'], loss=loss.item(), metrics=metrics)
 
     @abstractmethod
     def execute(self):
--- Runners/Trainer.py
+++ Runners/Trainer.py
@@ -7,7 +7,6 @@ from torch_geometric import loader as tg_loader
 import pandas as pd
 from collections import namedtuple
 from Runners.BaseRunner import BaseRunner
-from Tools.Dataset import Dataset
 from utils import utils
 
 log = logging.getLogger(__name__)
@@ -19,164 +18,36 @@ class Trainer(BaseRunner):
                 verbose=False, threshold=0.1, threshold_mode='rel',
                 cooldown=0, min_lr=1e-8, eps=1e-8)
 
-    def load(self):
-        super().load()
-        print(torchinfo.summary(self.network_wrapper.model))
-
-        # If pretrain, then load pretrained model
-        self.pretrain = self.runner_config.get('pretrain')
-        if self.pretrain:
-            self.network_wrapper.load_model(self.runner_config.get('pre_model_path'))
-
-        self.init_epoch_id = 0
-        self.num_epochs = self.runner_config.get('num_epochs')
-        self.lr = self.runner_config.get('learning_rate')
-        self.optimizer = torch.optim.Adam(self.network_wrapper.parameters(), lr=self.lr)
-        if self.pretrain:
-            self.load_run_record(self.runner_config.get('pre_run_record'))
-    
-    def load_data(self):
-        from collections import deque
-        self.data_config = self.config.get('data_config')
-        self.dataset_prefix = self.data_config.get('dataset_prefix')
-        self.fold_id, self.num_folds = self.data_config.get('fold_id'), self.data_config.get('num_folds')
-        self.signal_scale_factor=float(self.data_config.get('signal_scale_factor'))
-
-        # Get train / validation / test id
-        idx = deque(range(self.num_folds))
-        idx.rotate(self.fold_id)
-        idx_dict = {
-            # 'train': list(idx)[:-2],
-            'train': list(idx)[-3],
-            'validation': list(idx)[-2],
-            'test': list(idx)[-1],
-        }
-
-        # Get data loader
-        self.data_sets = {}
-        for key, idx in idx_dict.items():
-            # TODO: support a list of index
-            self.data_sets[key] = Dataset(f"{self.dataset_prefix}{idx}.pt", signal_scale_factor=self.signal_scale_factor)
-
     def execute(self):
         self.BestEpoch = namedtuple('BestEpoch', ['epoch', 'loss'])
         self.best_epoch = self.BestEpoch(0, float('inf'))
 
         epoch_start, epoch_end = self.init_epoch_id, self.init_epoch_id+self.num_epochs
         for epoch in range(epoch_start, epoch_end):
-            log.info(f"Epoch: {epoch}/{epoch_end-1}. Learning rate: {self.optimizer.param_groups[0]['lr']}")
+            self.begin_of_epoch()
             # train one epoch
-            log.info(f"Training...")
+            log.info(f"Training with learning rate: {self.optimizer.param_groups[0]['lr']}")
             start_time = time.time()
-            self.network_wrapper.train()
-            data_loader = tg_loader.DataLoader(self.data_sets.get('train'), batch_size=self.batch_size, shuffle=True)
-            for batch_id, batch in enumerate(data_loader, 0):
-                # clear the gradients of all optimized variables
-                self.optimizer.zero_grad()
-                # Model output
-                batch = batch.to(self.device)
-                output = self.network_wrapper(batch)
-                result = self.evaluator(output=output, truth_label=batch.y, weight=batch.weight)
-                # backward pass: compute gradient of the loss with respect to model parameters
-                result.loss.backward()
-                self.optimizer.step()
-                # record result
-                self.do_record(epoch=epoch, batch_type='train', batch_id=batch_id, batch_weight=batch.weight.sum(), **result._asdict())
+            self.train_model(data_loader=self.dataset.get('train'), epoch=epoch)
             log.info(f"Complete in {(time.time()-start_time)/60.:.2f} min.")
             
             # Validate one epoch
             log.info(f"Validating...")
             start_time = time.time()
             data_loader = tg_loader.DataLoader(self.data_sets.get('validation'), batch_size=self.batch_size, shuffle=False)
-            output_save = self.apply_model(data_loader=data_loader, epoch=epoch, batch_type='validation')
-            self.end_epoch(epoch=epoch, output=output_save)
+            output_save, truth_save, weight_save = self.apply_model(data_loader=data_loader, epoch=epoch, batch_type='validation')
             log.info(f"Complete in {(time.time()-start_time)/60.:.2f} min.")
-            log.info(f"Total time: {(time.time()-self.time_start)/60.:.2f} min.")
-            print('\n\n')
+            self.end_of_epoch(output=output_save, truth_label=truth_save, weight=weight_save, test_epoch=False)
 
         # Test model 
-        start_time = time.time()
         # get best epoch
         epoch = self.best_epoch.epoch
-        log.info(f"Testing model in epoch {epoch}...")
-        self.network_wrapper.load_model(f'{self.save_dir}/model.pt')
+        log.info(f"Testing model from epoch {epoch}...")
+        start_time = time.time()
+        self.network.load_model(f'{self.save_dir}/model.pt')
         data_loader = tg_loader.DataLoader(self.data_sets.get('test'), batch_size=self.batch_size, shuffle=False)
-        output_save = self.apply_model(data_loader=data_loader, epoch=epoch, batch_type='test')
-        # save score and record
-        np.save(f"{self.save_dir}/testset_output.npy",arr=output_save)
-        self.plot_loss()
-        self.plot_score(output=output_save)
-        log.info(f"Complete in {(time.time()-start_time)/60.:.2f} min.")
-        self.print_epoch_result(epoch=epoch)
-
-    """
-    Running at the end of each epoch
-    """
-    def end_epoch(self, epoch, output):
-        np.save(f"{self.save_dir}/valset_output.npy",arr=output)
-        self.save(record_path=f'{self.save_dir}/run_record.csv', model_path=f'{self.save_dir}/models/model{epoch}.pt')
-        self.plot_loss()
-        self.plot_score(output=output)
-
-        # Record best epoch
-        cur_epoch_result = self.search_record(self.run_record, epoch=epoch, batch_type='validation')
-        loss, weight = np.array(cur_epoch_result.get('loss')), np.array(cur_epoch_result.get('batch_weight'))
-        cur_loss = (loss*weight).sum() / weight.sum()
-        self.reduce_schedule.step(cur_loss)
-        if cur_loss < self.best_epoch.loss:
-            self.best_epoch = self.BestEpoch(epoch, cur_loss)
-            self.network_wrapper.save_model(f'{self.save_dir}/model.pt')
-        self.print_epoch_result(epoch=epoch)
-        log.info(f'Best epoch: {self.best_epoch.epoch} with loss {self.best_epoch.loss}')
-
-
-    def save(self, record_path, model_path):
-        record_df = pd.DataFrame(self.run_record)
-        record_df.to_csv(record_path, index=None)
-        self.network_wrapper.save_model(model_path)
+        output_save, truth_save, weight_save = self.apply_model(data_loader=data_loader, epoch=epoch, batch_type='test')
+        self.end_of_epoch(output=output_save, truth_label=truth_save, weight=weight_save, test_epoch=True)
 
     def finish(self):
         self.save(record_path=f'{self.save_dir}/run_record.csv', model_path=f'{self.save_dir}/model.pt')
-    
-    def plot_loss(self):
-        result = pd.DataFrame(self.run_record)
-        batch_types = result['batch_type'].unique()
-        result['loss'] = result['loss'] * result['batch_weight']
-        plot = utils.PlotContainer(xlabel='Epoch', ylabel='Loss',figname=f'{self.save_dir}/loss_to_epoch.pdf')
-        result = result.groupby(['batch_type', 'epoch']).sum()
-        result['loss'] = result['loss'] / result['batch_weight']
-
-        for btype in batch_types:
-            cur_result = result.loc[btype]
-            plot.ax.plot(cur_result.index, cur_result['loss'], linewidth=2, label=f"{btype} loss", marker='o')
-        plot.apply_settings(if_legend=True)
-        plot.savefig()
-    
-    def plot_score(self, output):
-        from scipy.special import softmax
-        from sklearn.metrics import roc_curve
-        weight = output[:,0]
-        truth = output[:,1]
-        score = softmax(output[:, 2:], axis=1)[:,1]
-        signal_mask = (truth==1)
-
-        # Plot scores
-        plot = utils.PlotContainer(xlabel='Score', ylabel='A.U.', figname=f'{self.save_dir}/scores.pdf')
-        bins = 50
-        plot.ax.hist(score[signal_mask], bins=bins, weights=weight[signal_mask], label=rf'Signal$\times${self.signal_scale_factor:.0f}', color='red', histtype='step', linewidth=2)
-        plot.ax.hist(score[~signal_mask], bins=bins, weights=weight[~signal_mask], label='Background', color='blue', histtype='step', linewidth=2)
-        plot.apply_settings()
-        plot.ax.legend(loc='upper center')
-        plot.savefig()
-
-        # Plot ROC
-        fpr, tpr, _ = roc_curve(truth, score, sample_weight=weight)
-        roc_auc = np.trapz(tpr, fpr)
-        log.info(f"auc: {roc_auc}.")
-        plot = utils.PlotContainer(xlabel='False Positive Rate', ylabel='True Positive Rate', figname=f'{self.save_dir}/roc.pdf', xlim=[0,1], ylim=[0,1])
-        plot.ax.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.3f)' % roc_auc)
-        plot.ax.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
-        plot.ax.legend(loc='lower right')
-        plot.apply_settings()
-        plot.savefig()
-        utils.plt.cla()
